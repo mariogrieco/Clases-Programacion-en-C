8e01a852eb6643c278753cf46f2d6a23523b35dd
Mario Grieco <mariojosuexz@gmail.com>
2016-07-02 18:48:41 -0400

    clase 4

--- Clase 4/main.c
+++ Clase 4/main.c
@@ -5,170 +5,189 @@
 #include "conio.h"
 #include "myconio.h"
 
-	
-		struct Jugador
-		{
-			int x;
-			int y;
-			char forma;
-		};
+		/* Prototypes */
+		void defaultColor(void);
+		void mprint(int, int);
+		void moveJugador(int, int);
+		void deleteJugador(void);
 
-
-		char option;
-
-		struct Jugador player;
-		struct Jugador contador;
-		int campo[20][20] = {
+		int campo[20][20] = { 
 								{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
 								{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-								{ 1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1 },
-								{ 1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1 },
-								{ 1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1 },
-								{ 1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,1 },
-								{ 1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1 },
-								{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-								{ 1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1 },
-								{ 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1 },
-								{ 1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1 },
-								{ 1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1 },
-								{ 1,1,0,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,1 },
-								{ 1,1,0,1,1,1,1,1,0,1,0,1,0,0,0,0,0,1,0,1 },
-								{ 1,1,0,0,0,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1 },
-								{ 1,1,1,1,0,0,1,1,0,1,0,1,0,1,1,1,0,1,0,1 },
-								{ 1,1,1,1,1,0,0,1,0,1,0,1,0,1,1,0,0,0,0,1 },
-								{ 1,1,1,1,1,1,0,0,0,1,0,1,0,1,1,1,1,1,0,1 },
-								{ 1,1,1,1,1,1,0,1,1,0,0,1,0,0,0,0,0,0,0,1 },
-								{ 1,1,1,1,1,1,9,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+								{ 1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1 },
+								{ 1,0,1,0,0,0,3,0,0,1,0,0,0,3,0,0,0,0,0,1 },
+								{ 1,0,1,0,1,0,1,1,1,1,0,1,1,1,0,1,1,1,0,1 },
+								{ 1,0,1,0,1,0,0,0,1,0,0,1,1,1,0,1,1,1,0,1 },
+								{ 1,0,1,0,1,1,1,0,1,0,1,0,0,0,0,1,1,1,0,1 },
+								{ 1,0,1,0,0,0,1,0,1,0,1,0,1,1,1,1,1,1,0,1 },
+								{ 1,0,1,1,1,0,1,0,1,0,1,0,1,1,1,1,1,1,0,1 },
+								{ 1,0,1,1,1,0,1,0,1,0,1,0,0,0,0,0,0,1,0,1 },
+								{ 1,0,0,0,0,0,1,0,1,0,1,0,1,1,1,1,1,1,0,1 },
+								{ 1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,0,1 },
+								{ 1,0,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,1,0,1 },
+								{ 1,3,1,3,0,0,0,0,1,0,1,0,1,1,1,1,0,1,0,1 },
+								{ 1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1 },
+								{ 1,0,0,0,0,1,1,0,1,0,0,0,0,0,0,1,1,1,0,1 },
+								{ 1,0,1,1,0,1,1,0,1,0,1,1,1,1,0,1,1,1,0,1 },
+								{ 1,0,1,1,0,1,1,3,1,3,1,1,1,1,3,1,1,1,0,1 },
+								{ 1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+								{ 1,9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
 							};
+		char option;
 
+		struct Player
+		{
+			int filas;
+			int columnas;
+			char cuerpo;
+		};
 
-		void mprint(int[][20],int,int);
-		void deletePlayer(void);
-		void printPlayer(int x, int y);
+		struct Player jugador;
+		struct Player helper;
+
 
 int main()
-{
-		
-		player.x = 0;
-		player.y = 1;
-		player.forma = 'X';
-		contador.x = 0;
-		contador.y = 1;
-
+{		
 	system("cls");
-	textbgcolor(WHITE,BLACK);
-	mprint(campo,20,20);
+		jugador.filas = 1;
+		jugador.columnas = 0;
+		jugador.cuerpo = 'x';
 
-	gotoxy(player.x,player.y);
-	printf("%c",player.forma);
-		
-	while( 1 == 1 ){
+		helper.filas = 1;
+		helper.columnas = 0;
+
+		mprint(20,20);
+	
+		while ( 1 == 1 )
+		{
+			moveJugador(0,0);
 			option = getch();
-			
-			if ( option == 'q' )
+
+			if (  option == 113 /* es igual 'q' */	)
 			{
 				return 0;
 			}
-			if ( option == 100 /*100 == d*/ )
+			if ( option == 'd' )
 			{
-			
-				if ( campo[contador.y][contador.x+1] == 0 && ( contador.x+1 < 20))
+				if ( campo[helper.filas][helper.columnas+1] == 0 && helper.columnas < 20 )
 				{
-					contador.x += 1;
-					deletePlayer();
-					printPlayer(+2,0);
-					if ( campo[contador.y][contador.x+1] == 9 )
-					{
-						system("cls");
-						printf("you win! :)");
-					}
+					helper.columnas += 1;
+					deleteJugador();
+					moveJugador(+2,0);
+				}
+				if ( campo[helper.filas][helper.columnas+1] == 9	)
+				{
+					printf("(YOU WIN! XD)!");
+				}
+				if (campo[helper.filas][helper.columnas+1]  == 3)
+				{
+					printf("BOOM! eso era una mina you lose sorry xD\n");
 				}
 			}
-			if ( option == 'a' /*100 == d*/ )
+			if ( option == 'a' )
 			{
-			
-				if ( campo[contador.y][contador.x-1] == 0 && ( contador.x+1 >= 0))
+				if ( campo[helper.filas][helper.columnas-1] == 0 && helper.columnas >= 0 )
 				{
-					contador.x -= 1;
-					deletePlayer();
-					printPlayer(-2,0);
-					if ( campo[contador.y][contador.x-1] == 9 )
-					{
-						system("cls");
-						printf("you win! :)");
-					}
+					helper.columnas -= 1;
+					deleteJugador();
+					moveJugador(-2,0);
+				}
+				if ( campo[helper.filas][helper.columnas-1] == 9	)
+				{
+					printf("(YOU WIN! XD)!");
+				}
+				if (campo[helper.filas][helper.columnas-1]  == 3)
+				{
+					printf("BOOM! eso era una mina you lose sorry xD\n");
 				}
 			}
 			if ( option == 'w' )
 			{
-				if ( campo[contador.y-1][contador.x] == 0 && contador.y-1 >= 0 )
+				if ( campo[helper.filas-1][helper.columnas] == 0 && helper.filas >= 0 )
 				{
-					contador.y -= 1;
-					deletePlayer();
-					printPlayer(0,-1);
-					if ( campo[contador.y-1][contador.x] == 9 )
-					{
-						system("cls");
-						printf("you win! :)");
-					}
+					helper.filas -= 1;
+					deleteJugador();
+					moveJugador(0,-1);
+				}
+				if ( campo[helper.filas-1][helper.columnas] == 9	)
+				{
+					printf("(YOU WIN! XD)!");
+				}
+				if (campo[helper.filas-1][helper.columnas] == 3)
+				{
+					printf("BOOM! eso era una mina you lose sorry xD\n");
 				}
 			}
 			if ( option == 's' )
 			{
-				if ( campo[contador.y+1][contador.x] == 0 && contador.y+1 < 20 )
+				if ( campo[helper.filas+1][helper.columnas] == 0 && helper.filas < 20 )
 				{
-					contador.y += 1;
-					deletePlayer();
-					printPlayer(0,+1);
-					if ( campo[contador.y+1][contador.x] == 9 )
-					{
-						system("cls");
-						printf("you win! :)");
-					}
+					helper.filas += 1;
+					deleteJugador();
+					moveJugador(0,+1);
 				}
-
+				if ( campo[helper.filas+1][helper.columnas] == 9	)
+				{
+					printf("(YOU WIN! XD)!");
+				}
+				if (campo[helper.filas+1][helper.columnas] == 3)
+				{
+					printf("BOOM! eso era una mina you lose sorry xD\n");
+				}
 			}
-	}
-}
 
-void mprint(int campo[][20],int f,int c ){
-	int i,x;
-
-	textbgcolor(WHITE,BLACK);
-	for ( i = 0; i < 20; i++)
-	{
-		for ( x = 0; x < 20; x++)
-		{
-			if (campo[i][x] == 0 || campo[i][x] == 9 )
-			{
-				textbgcolor(BLACK,BLACK);
-				printf("%i ",campo[i][x]);
-			}
-			else
-			{
-				textbgcolor(1,1);
-				printf("%i ",campo[i][x]);
-			}
 		}
-		printf("\n");
+}
+
+void defaultColor(void){
+	textbgcolor(WHITE,BLACK);
+}
+
+void mprint(int f, int c){
+	int x,i;
+
+	defaultColor();
+	for ( x = 0; x < f; x++)
+	{
+		for ( i = 0; i < c; i++)
+		{
+			if ( campo[x][i] == 0 || campo[x][i] == 9 )
+			{
+				/* print caminos */
+				textbgcolor(BLACK,BLACK);
+				printf("%i ",campo[x][i]);
+			}
+			else if ( campo[x][i] == 1 )
+			{
+				/* print pare */
+				textbgcolor(BLUE,BLUE);
+				printf("%i ",campo[x][i]);
+			}	
+			else if ( campo[x][i] == 3 )
+			{
+				/* print mina */
+				textbgcolor(GRAY,GRAY);
+				printf("%i ",campo[x][i]);
+			}	
+		}
+			printf("\n");
 	}
-	textbgcolor(WHITE,BLACK);
-}
-
-void deletePlayer(void){
-		gotoxy(player.x,player.y);
-		textbgcolor(WHITE,BLACK);
-		printf("  ");
+	defaultColor();
 
 }
 
-void printPlayer(int x, int y){
-    
-    player.y += y;
-    player.x += x;
-    textbgcolor(GREEN,GREEN);
-	gotoxy(player.x,player.y);
+void moveJugador(int x, int y){
+
+	jugador.filas += y;
+	jugador.columnas += x;
+
+	textbgcolor(GREEN,GREEN);
+	gotoxy(jugador.columnas,jugador.filas);
+	printf("%c ",jugador.cuerpo);
+	defaultColor();
+}
+void deleteJugador(void){
+	gotoxy(jugador.columnas,jugador.filas);
+	defaultColor();
 	printf("  ");
-
-	textbgcolor(WHITE,BLACK);
 }
